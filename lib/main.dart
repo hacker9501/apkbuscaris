155b48de89ad1ad923022e976944f9ab19bda235
hacker9501 <holahacker01@gmail.com>
2022-11-16 16:36:40 -0500

    apkbuscaris

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return WebView(
+      initialUrl: "https://buscaris.com/",
+      javascriptMode: JavascriptMode.unrestricted,
+    );
+  }
+}
+
